fd879db332112521c47c78851a86237ce3e4ad4d
W <https://github.com/WW2131/home_work.git>
2020-12-26 11:04:20 +0800

    v 1.2

--- src/main/java/day05/Emp.java
+++ src/main/java/day05/Emp.java
@@ -1,4 +1,10 @@
 package day05;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * 定义私有属性:
  * String name;
@@ -17,6 +23,94 @@ package day05;
  * @author Bonnie
  *
  */
-public class Emp {
+public class Emp implements Comparable {
 
+    String name;
+    int age;
+    String gender;
+    int salary;
+    Date hiredate;//入职时间
+
+
+    @Override
+    public String toString() {
+
+        SimpleDateFormat simpleDateFormat  = new SimpleDateFormat("yyyy-MM-dd");
+        String str = simpleDateFormat.format(this.hiredate);
+        return "姓名：'" + name + '\'' +
+                ", 年龄：" + age +
+                ", 性别：'" + gender + '\'' +
+                ", 薪资：" + salary +
+                ", 入职时间：" + str+
+                ",转正时间："+simpleDateFormat.format(dateTime(this.hiredate).getTime());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        day04.Emp emp = (day04.Emp) obj;
+        return ((Emp) obj).age==this.age;
+    }
+
+
+
+    public Emp(String name, int age, String gender, int salary, Date hiredate) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.hiredate = hiredate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Calendar dateTime(Date date){
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH,3);
+        calendar.set(Calendar.DAY_OF_WEEK,6);
+        return calendar;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return ((Emp)o).getAge()-this.age;
+    }
 }
--- src/main/java/day05/Test01.java
+++ src/main/java/day05/Test01.java
@@ -1,4 +1,10 @@
 package day05;
+
+import javax.sound.sampled.Line;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
 /**
  * 创建一个队列，存入Integer类型元素1,2,3,4,5
  * 然后遍历队列并输出每个元素
@@ -6,5 +12,13 @@ package day05;
  *
  */
 public class Test01 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        Integer[] arr = {1,2,3,4,5};
+        queue.addAll(Arrays.asList(arr));
+        for (Integer integer : queue) {
+            System.out.println(integer);
+        }
 
+    }
 }
